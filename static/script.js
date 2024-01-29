0e57af2d7e2a57fda0b3680d56445c29b9b2c6eb
lorenzrock <lorockstuhl@gmil.com>
2024-01-29 20:06:29 +0100

    added the weapon and charakter selecter

--- static/script.js
+++ static/script.js
@@ -18,9 +18,133 @@ canvas.height = 576
 let timerId
 let timer = 60
 
+player_selection = document.getElementById("player-selection")
+
+
+
 // add eventlistener
 start_1v1.addEventListener("click", start1v1Game);
 end_game_btn.addEventListener("click", reset_game);
+
+
+
+spirit_src_list = ["/static/assets/ninja_green.png", "/static/assets/ninja_red.png", "/static/assets/badman.png", "/static/assets/spidy.png"]
+// 0 green ninja, 1 red ninja, 2 badman, 3 spidy
+
+// Set default
+player1_selected_spirit = spirit_src_list[0]
+player2_selected_spirit = spirit_src_list[1]
+
+// could i use Case?
+green_ninja_png_p1 = document.getElementById("green_ninja_png_p1")
+green_ninja_png_p1.addEventListener("click",() => {
+    player1.img.src = spirit_src_list[0]
+    console.log(player1_selected_spirit)
+})
+red_ninja_png_p1 = document.getElementById("red_ninja_png_p1")
+red_ninja_png_p1.addEventListener("click",() => {
+    player1.img.src = spirit_src_list[1]
+    console.log(player1_selected_spirit)
+})
+badman_ninja_png_p1 = document.getElementById("badman_ninja_png_p1")
+badman_ninja_png_p1.addEventListener("click",() => {
+    player1.img.src = spirit_src_list[2]
+    console.log(player1_selected_spirit)
+})
+spidy_png_p1 = document.getElementById("spidy_png_p1")
+spidy_png_p1.addEventListener("click",() => {
+    player1.img.src = spirit_src_list[3]
+    console.log(player1_selected_spirit)
+})
+
+
+green_ninja_png_p2 = document.getElementById("green_ninja_png_p2")
+green_ninja_png_p2.addEventListener("click",() => {
+    player2.img.src = spirit_src_list[0]
+    console.log(player2_selected_spirit)
+})
+red_ninja_png_p2 = document.getElementById("red_ninja_png_p2")
+red_ninja_png_p2.addEventListener("click",() => {
+    player2.img.src = spirit_src_list[1]
+    console.log(player2_selected_spirit)
+})
+badman_ninja_png_p2 = document.getElementById("badman_ninja_png_p2")
+badman_ninja_png_p2.addEventListener("click",() => {
+    player2.img.src = spirit_src_list[2]
+    console.log(player2_selected_spirit)
+})
+spidy_png_p2 = document.getElementById("spidy_png_p2")
+spidy_png_p2.addEventListener("click",() => {
+    player2.img.src = spirit_src_list[3]
+    console.log(player2_selected_spirit)
+})
+
+
+weapon_src_list = ["static/assets/weapons/1.png", "static/assets/weapons/2.png", "static/assets/weapons/3.png", "static/assets/weapons/4.png"]
+
+// set deafult weapon
+player1_selected_weapon = weapon_src_list[0]
+player2_selected_weapon = weapon_src_list[1]
+
+
+weapon_1_p1 = document.getElementById("weapon_1_p1")
+weapon_1_p1.addEventListener("click",() => {
+    player1.weapon_img.src = weapon_src_list[0]
+    console.log(weapon_src_list[0])
+})
+weapon_2_p1 = document.getElementById("weapon_2_p1")
+weapon_2_p1.addEventListener("click",() => {
+    player1.weapon_img.src = weapon_src_list[1]
+    console.log(weapon_src_list[1])
+})
+weapon_3_p1 = document.getElementById("weapon_3_p1")
+weapon_3_p1.addEventListener("click",() => {
+    player1.weapon_img.src = weapon_src_list[2]
+    console.log(weapon_src_list[2])
+})
+weapon_4_p1 = document.getElementById("weapon_4_p1")
+weapon_4_p1.addEventListener("click",() => {
+    player1.weapon_img.src = weapon_src_list[3]
+    console.log(weapon_src_list[3])
+})
+
+
+
+weapon_1_p2 = document.getElementById("weapon_1_p2")
+weapon_1_p2.addEventListener("click",() => {
+    player2.weapon_img.src = weapon_src_list[0]
+    console.log(weapon_src_list[0])
+})
+weapon_2_p2 = document.getElementById("weapon_2_p2")
+weapon_2_p2.addEventListener("click",() => {
+    player2.weapon_img.src = weapon_src_list[1]
+    console.log(weapon_src_list[1])
+})
+weapon_3_p2 = document.getElementById("weapon_3_p2")
+weapon_3_p2.addEventListener("click",() => {
+    player2.weapon_img.src = weapon_src_list[2]
+    console.log(weapon_src_list[2])
+})
+weapon_4_p2 = document.getElementById("weapon_4_p2")
+weapon_4_p2.addEventListener("click",() => {
+    player2.weapon_img.src = weapon_src_list[3]
+    console.log(weapon_src_list[3])
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -107,7 +231,7 @@ window.addEventListener("keyup", (e) => {
 })
 
 class Sprite {
-    constructor({position, velocity, color = "red", offset, img}) {
+    constructor({position, velocity, color = "red", offset, img, weapon_img}) {
         this.position = position
         this.velocity = velocity
         this.width = 50
@@ -127,6 +251,7 @@ class Sprite {
         this.color = color
         this.isAttacking
         this.img = img
+        this.weapon_img = weapon_img
 
     }
     draw() {
@@ -136,8 +261,9 @@ class Sprite {
         c.drawImage(this.img, this.position.x, this.position.y, this.width, this.height)
         // attackBox
         if (this.isAttacking) {
-            c.fillStyle = "green"
-            c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.with, this.attackBox.height)       
+            // c.fillStyle = "green"
+            // c.fillRect(this.attackBox.position.x, this.attackBox.position.y, this.attackBox.with, this.attackBox.height)       
+            c.drawImage(this.weapon_img, this.attackBox.position.x, this.attackBox.position.y, this.attackBox.with, this.attackBox.height)
         }
     }
     update() {
@@ -174,10 +300,12 @@ const player1 = new Sprite({
         x: 0,
         y: 0
     },
-    img: new Image()
+    img: new Image(),
+    weapon_img: new Image()
     
 })
-player1.img.src = "static/assets/ninja_green.png"
+player1.img.src = player1_selected_spirit
+player1.weapon_img.src = player1_selected_weapon
 const player2 = new Sprite({
     position: {
         x: 950,
@@ -192,14 +320,15 @@ const player2 = new Sprite({
         y: 0
     },
     color: "blue",
-    img: new Image()
+    img: new Image(),
+    weapon_img: new Image()
 })
-player2.img.src = "static/assets/pixilart-drawing (3).png"
+player2.img.src = player2_selected_spirit
+player2.weapon_img.src = player2_selected_weapon
 // Important change agording do player postion
-function settplyer2Offset() {
+function settplyer2Offset() { // this is for so the attackbox positions to face the player
     if (
         player1.width/2 + player1.position.x > player2.position.x + player2.width/2
-        // denk dran wo x ist 
     ) {
         player1.attackBox.offset.x = -50
         player2.attackBox.offset.x = 0
@@ -272,7 +401,7 @@ function animate() {
 
         determineWinner({player1, player2, timerId})   
     }
-/////////////////////// rectangle1.isAttacking
+///////////////////////////////////////////////////////////// rectangle1.isAttacking
 }
 
 
@@ -343,46 +472,6 @@ function reset_game() {
 
 
 
-
-
-function select_spirit() {
-    var player1_selected = false
-    var player2_selected = false
-
-    function set_selected_player() {
-        player1_selected
-    }
-    
-    
-    selected_player1 = document.getElementById("selected_player1")
-    selected_player2 = document.getElementById("selected_player2")
-
-    selected_player1.addEventListener("click", () => {
-        player2_selected = true
-    })
-    selected_player2.addEventListener("click", () => {
-        player1_selected = true
-    })
-
-
-    // if (player2_selected) {
-        // player1.img.src = spirits[img_id]
-    // }
-    // if (player2_selected) {
-        // player1.img.src = spirits[img_id]
-    // }
-
-
-
-
-    var player1_selected = false
-    var player2_selected = false
-}
-
-
-
-
-
 function start1v1Game() {
     if (start_1v1.style.display = "block") {
         start_1v1.style.display = "none";    
@@ -391,8 +480,7 @@ function start1v1Game() {
         start_1v1.style.display = "block";    
         canvas.style.display = "none";
     }
-    select_spirit()
-
+    player_selection.style.display = "none";
 
 
     animate();
